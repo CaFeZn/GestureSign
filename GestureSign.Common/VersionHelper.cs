c264ef0344f236223e60ddb3d8ca84c4439cf061
CaFeZn <1837781998@qq.com>
2026-06-07 15:21:16 +0800

    Use native Windows version detection

--- GestureSign.Common/VersionHelper.cs
+++ GestureSign.Common/VersionHelper.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
+using System.Runtime.InteropServices;
 
 namespace GestureSign.Common
 {
     public class VersionHelper
     {
+        private const int StatusSuccess = 0;
         private static Version _osVersion;
 
         public static Version OsVersion
@@ -12,7 +14,7 @@ namespace GestureSign.Common
             {
                 if (_osVersion == null)
                 {
-                    _osVersion = Environment.OSVersion.Version;
+                    _osVersion = GetOsVersion();
                 }
                 return _osVersion;
             }
@@ -35,7 +37,57 @@ namespace GestureSign.Common
 
         public static bool IsWindows10OrGreater()
         {
-            return OsVersion.Major >= 10;
+            return OsVersion >= new Version(10, 0);
+        }
+
+        public static bool IsWindows11OrGreater()
+        {
+            return OsVersion >= new Version(10, 0, 22000);
+        }
+
+        private static Version GetOsVersion()
+        {
+            try
+            {
+                var versionInfo = new OSVERSIONINFOEX();
+                versionInfo.dwOSVersionInfoSize = Marshal.SizeOf(typeof(OSVERSIONINFOEX));
+
+                if (RtlGetVersion(ref versionInfo) == StatusSuccess)
+                {
+                    return new Version(
+                        versionInfo.dwMajorVersion,
+                        versionInfo.dwMinorVersion,
+                        versionInfo.dwBuildNumber);
+                }
+            }
+            catch (DllNotFoundException)
+            {
+            }
+            catch (EntryPointNotFoundException)
+            {
+            }
+
+            return Environment.OSVersion.Version;
+        }
+
+        [DllImport("ntdll.dll")]
+        private static extern int RtlGetVersion(ref OSVERSIONINFOEX versionInfo);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct OSVERSIONINFOEX
+        {
+            public int dwOSVersionInfoSize;
+            public int dwMajorVersion;
+            public int dwMinorVersion;
+            public int dwBuildNumber;
+            public int dwPlatformId;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string szCSDVersion;
+            public ushort wServicePackMajor;
+            public ushort wServicePackMinor;
+            public ushort wSuiteMask;
+            public byte wProductType;
+            public byte wReserved;
         }
     }
 }
