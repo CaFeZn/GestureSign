cb7af5c1539f666a44a0729cc67a1c17169556e8
CaFeZn <1837781998@qq.com>
2026-06-08 21:58:35 +0800

    Stabilize live multi-touch point ordering

--- GestureSign.Daemon/Input/PointCapture.cs
+++ GestureSign.Daemon/Input/PointCapture.cs
@@ -1007,8 +1007,17 @@ namespace GestureSign.Daemon.Input
             }
             if (getNewPoint)
             {
+                var currentPointsByContact = point
+                    .GroupBy(p => p.ContactIdentifier)
+                    .ToDictionary(group => group.Key, group => group.Last().Point);
+                var latestPoints = _pointsCaptured
+                    .Select(capturedPoint => currentPointsByContact.TryGetValue(capturedPoint.Key, out var currentPoint)
+                        ? currentPoint
+                        : capturedPoint.Value.Last())
+                    .ToList();
+
                 // Notify subscribers that point has been captured
-                OnPointCaptured(new PointsCapturedEventArgs(new List<List<Point>>(_pointsCaptured.Values), point.Select(p => p.Point).ToList()));
+                OnPointCaptured(new PointsCapturedEventArgs(new List<List<Point>>(_pointsCaptured.Values), latestPoints));
             }
         }
 
